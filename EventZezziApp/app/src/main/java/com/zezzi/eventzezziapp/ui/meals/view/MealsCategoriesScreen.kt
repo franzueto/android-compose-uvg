8995816eee226c8ca2d2b1978db5e2b693f36ba0
LeivaDiego <lei21752@uvg.edu.gt>
2023-10-08 09:02:11 -0600

    UI Aesthetic Improvement
    Enhanced Card design.
    Detailed description dialog. Refactored code for clarity.

--- EventZezziApp/app/src/main/java/com/zezzi/eventzezziapp/ui/meals/view/MealsCategoriesScreen.kt
+++ EventZezziApp/app/src/main/java/com/zezzi/eventzezziapp/ui/meals/view/MealsCategoriesScreen.kt
@@ -1,18 +1,26 @@
 package com.zezzi.eventzezziapp.ui.meals.view
 
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.AlertDialog
 import androidx.compose.material.Card
 import androidx.compose.material.Text
+import androidx.compose.material.TextButton
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
@@ -23,8 +31,8 @@ import com.zezzi.eventzezziapp.navigation.AppBar
 @Composable
 fun MealsCategoriesScreen(
     navController: NavController,
-    viewModel: MealsCategoriesViewModel = viewModel()){
-
+    viewModel: MealsCategoriesViewModel = viewModel())
+{
     LaunchedEffect(Unit) {
         viewModel.getMeals()
     }
@@ -34,24 +42,58 @@ fun MealsCategoriesScreen(
         LazyVerticalGrid(
             columns = GridCells.Fixed(2),
             contentPadding = it,
-            content = {items(viewModel.meals.value){meal -> CategoryCard(meal)}})
+            content = { items(viewModel.meals.value) { meal -> CategoryCard(meal) } }
+        )
     }
 }
 
 @Composable
-fun CategoryCard(meal: MealResponse){
-    Card (elevation = 8.dp
-        , modifier = Modifier
+fun CategoryCard(meal: MealResponse) {
+    var showDialog by remember { mutableStateOf(false) }
+
+    Card(
+        elevation = 10.dp,
+        modifier = Modifier
             .padding(8.dp)
             .fillMaxWidth()
-            .clickable { /* Navigation Here */ })
-    {
-        Column(modifier = Modifier.padding(16.dp))
-        {
-            AsyncImage(model = meal.imageUrl, contentDescription = "Category Image")
-            Spacer(modifier = Modifier.height(8.dp))
-            Text(text = meal.name)
-            Text(text = meal.description)
+            .height(250.dp)
+            .clickable { showDialog = true },
+        shape = RoundedCornerShape(16.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            AsyncImage(
+                model = meal.imageUrl,
+                contentDescription = "Category Image",
+                modifier = Modifier.size(120.dp)
+            )
+            Text(text = meal.name, fontSize = 30.sp, fontWeight = FontWeight.Bold)
+            Text(text = "${meal.description.take(50)}...", fontSize = 15.sp)
         }
     }
+
+    if (showDialog) {
+        ShowDescriptionDialog(meal) {
+            showDialog = false
+        }
+    }
+}
+
+@Composable
+fun ShowDescriptionDialog(meal: MealResponse, onClose: () -> Unit) {
+    AlertDialog(
+        onDismissRequest = onClose,
+        title = { Text(text = meal.name, fontSize = 24.sp) },
+        text = { Text(text = meal.description, fontSize = 15.sp) },
+        confirmButton = {
+            TextButton(onClick = onClose) {
+                Text("Ok")
+            }
+        },
+        shape = RoundedCornerShape(16.dp),
+        modifier = Modifier.border(2.dp, Color.Gray)
+    )
 }
