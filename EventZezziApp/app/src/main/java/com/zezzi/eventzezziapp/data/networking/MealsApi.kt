a62b6c00ecebd6ca8fdd00cd48da3520dbf3bac2
JPOrellana <115191781+JPOrellana@users.noreply.github.com>
2023-10-08 16:11:00 -0600

    Inciso 11
    En esta acción realizamos lo indicado en el inciso 11 del laboratorio y esto corresponde a realizar el cambio de tipo de dato a retornar ya que debe ser simplemente MealsCategoriesResponse en
    vez de Call<MealsCategoriesResponse>

--- EventZezziApp/app/src/main/java/com/zezzi/eventzezziapp/data/networking/MealsApi.kt
+++ EventZezziApp/app/src/main/java/com/zezzi/eventzezziapp/data/networking/MealsApi.kt
@@ -5,6 +5,8 @@ import retrofit2.Call
 import retrofit2.http.GET
 
 interface MealsApi {
+
     @GET("categories.php")
-    fun getMeals(): Call<MealsCategoriesResponse>
+    suspend fun getMeals(): MealsCategoriesResponse
+
 }
