2d57b07b083c734b3f216b7d9711d998544867f2
Andyfer004 <99309349+Andyfer004@users.noreply.github.com>
2023-10-07 21:28:26 -0600

    convertir proyecto a courotines

--- EventZezziApp/app/src/main/java/com/zezzi/eventzezziapp/data/repository/MealsRepository.kt
+++ EventZezziApp/app/src/main/java/com/zezzi/eventzezziapp/data/repository/MealsRepository.kt
@@ -1,25 +1,23 @@
-package com.zezzi.eventzezziapp.data.repository
-
 import com.zezzi.eventzezziapp.data.networking.MealsWebService
 import com.zezzi.eventzezziapp.data.networking.response.MealsCategoriesResponse
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class MealsRepository(private val webService: MealsWebService = MealsWebService()) {
-    fun getMeals(successCallback: (response: MealsCategoriesResponse?) -> Unit) {
-        return webService.getMeals().enqueue(object : Callback<MealsCategoriesResponse> {
-            override fun onResponse(
-                call: Call<MealsCategoriesResponse>,
-                response: Response<MealsCategoriesResponse>
-            ) {
-                if (response.isSuccessful)
-                    successCallback(response.body())
+    suspend fun getMeals(): MealsCategoriesResponse? {
+        return try {
+            withContext(Dispatchers.IO) {
+                val response = webService.getMeals().execute()
+                if (response.isSuccessful) {
+                    response.body()
+                } else {
+                    // Manejar el error de alguna manera, por ejemplo, lanzar una excepción personalizada.
+                    null
+                }
             }
-
-            override fun onFailure(call: Call<MealsCategoriesResponse>, t: Throwable) {
-                // TODO treat failure
-            }
-        })
+        } catch (e: Exception) {
+            // Manejar la excepción de alguna manera, por ejemplo, lanzar una excepción personalizada.
+            null
+        }
     }
 }
--- EventZezziApp/app/src/main/java/com/zezzi/eventzezziapp/ui/meals/view/MealsCategoriesViewModel.kt
+++ EventZezziApp/app/src/main/java/com/zezzi/eventzezziapp/ui/meals/view/MealsCategoriesViewModel.kt
@@ -1,13 +1,21 @@
 package com.zezzi.eventzezziapp.ui.meals.view
 
+import MealsRepository
 import androidx.lifecycle.ViewModel
 import com.zezzi.eventzezziapp.data.networking.response.MealsCategoriesResponse
-import com.zezzi.eventzezziapp.data.repository.MealsRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class MealsCategoriesViewModel(private val repository: MealsRepository = MealsRepository()): ViewModel() {
-    fun getMeals(successCallback: (response: MealsCategoriesResponse?) -> Unit) {
-        repository.getMeals { response ->
-            successCallback(response)
+    suspend fun getMeals(): MealsCategoriesResponse? {
+        return withContext(Dispatchers.IO) {
+            try {
+                // Llamar a la función suspendida en el repositorio para obtener los datos.
+                repository.getMeals()
+            } catch (e: Exception) {
+                // Manejar errores si es necesario.
+                null
+            }
         }
     }
 }
