cf60fce8b716e9ccbc9753e82040a6b52abc69a6
Gohan1233 <gar21352@uvg.edu.gt>
2023-10-08 18:03:30 -0600

    Creacion de las cards

--- EventZezziApp/app/src/main/java/com/zezzi/eventzezziapp/ui/meals/view/MealsCategoriesScreen.kt
+++ EventZezziApp/app/src/main/java/com/zezzi/eventzezziapp/ui/meals/view/MealsCategoriesScreen.kt
@@ -1,14 +1,23 @@
 package com.zezzi.eventzezziapp.ui.meals.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.zezzi.eventzezziapp.navigation.AppBar
@@ -33,9 +42,21 @@ fun MealsCategoriesScreen(
             AppBar(title = "Categories", navController = navController)
         }
     ) {
-        LazyColumn(contentPadding = it) {
+        LazyColumn(
+            contentPadding = it,
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
             items(rememberedMeals.value) { meal ->
-                Text(text = meal.name)
+                Card (
+                    modifier = Modifier.fillMaxWidth(),
+                    shape = RoundedCornerShape(8.dp)
+                ){
+                    Text(
+                        text = meal.name,
+                        modifier = Modifier.padding(16.dp)
+                    )
+                }
+
             }
         }
     }
