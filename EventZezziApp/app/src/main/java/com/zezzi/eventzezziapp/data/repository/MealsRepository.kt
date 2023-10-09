c9a3cd7410fcb00bdff7c1a8c8e3d5c1af5cfeda
LeivaDiego <lei21752@uvg.edu.gt>
2023-10-09 08:25:53 -0600

    Optimization of getMeals Function
    Optimized the getMeals function in MealsRepository by using 'withContext' to target the IO thread.

--- EventZezziApp/app/src/main/java/com/zezzi/eventzezziapp/data/repository/MealsRepository.kt
+++ EventZezziApp/app/src/main/java/com/zezzi/eventzezziapp/data/repository/MealsRepository.kt
@@ -2,9 +2,11 @@ package com.zezzi.eventzezziapp.data.repository
 
 import com.zezzi.eventzezziapp.data.networking.MealsWebService
 import com.zezzi.eventzezziapp.data.networking.response.MealsCategoriesResponse
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class MealsRepository(private val webService: MealsWebService = MealsWebService()) {
     suspend fun getMeals(): MealsCategoriesResponse {
-        return webService.getMeals()
+        return withContext(Dispatchers.IO) {webService.getMeals()}
     }
 }
