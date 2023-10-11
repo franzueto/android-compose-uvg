b7ebe2e3ff8b60f25e9776c2f9ce4f31c37c9144
emiliosolanoo21 <85262580+emiliosolanoo21@users.noreply.github.com>
2023-10-10 22:45:22 -0600

    Coroutines

--- EventZezziApp/app/src/main/java/com/zezzi/eventzezziapp/ui/meals/view/MealsCategoriesScreen.kt
+++ EventZezziApp/app/src/main/java/com/zezzi/eventzezziapp/ui/meals/view/MealsCategoriesScreen.kt
@@ -1,18 +1,30 @@
+
 package com.zezzi.eventzezziapp.ui.meals.view
 
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.Card
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
-import com.zezzi.eventzezziapp.navigation.AppBar
 import com.zezzi.eventzezziapp.data.networking.response.MealResponse
+import com.zezzi.eventzezziapp.navigation.AppBar
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -21,11 +33,10 @@ fun MealsCategoriesScreen(
     viewModel: MealsCategoriesViewModel = viewModel()
 ) {
     val rememberedMeals: MutableState<List<MealResponse>> =
-        remember { mutableStateOf(emptyList<MealResponse>()) }
+        remember { mutableStateOf(emptyList()) }
 
-    viewModel.getMeals { response ->
-        val mealsFromTheApi = response?.categories
-        rememberedMeals.value = mealsFromTheApi.orEmpty()
+    LaunchedEffect(Unit) {
+        viewModel.getMeals()
     }
 
     Scaffold(
@@ -33,10 +44,24 @@ fun MealsCategoriesScreen(
             AppBar(title = "Categories", navController = navController)
         }
     ) {
-        LazyColumn(contentPadding = it) {
-            items(rememberedMeals.value) { meal ->
-                Text(text = meal.name)
-            }
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            contentPadding = it,
+            content = { items(viewModel.meals.value) { meal -> ShowedCard(meal) } }
+        )
+    }
+}
+
+@Composable
+fun ShowedCard(meal: MealResponse) {
+    Card() {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = meal.name, fontSize = 35.sp, fontWeight = FontWeight.SemiBold)
+            Text(text = "${meal.description.take(50)}...", fontSize = 20.sp)
         }
     }
 }
--- EventZezziApp/app/src/main/java/com/zezzi/eventzezziapp/ui/meals/view/MealsCategoriesViewModel.kt
+++ EventZezziApp/app/src/main/java/com/zezzi/eventzezziapp/ui/meals/view/MealsCategoriesViewModel.kt
@@ -1,13 +1,23 @@
 package com.zezzi.eventzezziapp.ui.meals.view
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
-import com.zezzi.eventzezziapp.data.networking.response.MealsCategoriesResponse
+import androidx.lifecycle.viewModelScope
+import com.zezzi.eventzezziapp.data.networking.response.MealResponse
 import com.zezzi.eventzezziapp.data.repository.MealsRepository
+import kotlinx.coroutines.launch
 
 class MealsCategoriesViewModel(private val repository: MealsRepository = MealsRepository()): ViewModel() {
-    fun getMeals(successCallback: (response: MealsCategoriesResponse?) -> Unit) {
-        repository.getMeals { response ->
-            successCallback(response)
+
+    val meals: MutableState<List<MealResponse>> = mutableStateOf(emptyList())
+
+    fun getMeals() {
+        viewModelScope.launch {
+            val response = repository.getMeals()
+            meals.value = response.categories
         }
     }
 }
+
+
