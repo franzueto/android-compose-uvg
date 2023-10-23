040a60a22d0f2102c3e51c8fe3c3bd3916367e1b
Andyfer004 <99309349+Andyfer004@users.noreply.github.com>
2023-10-23 00:52:00 -0600

    se agrego un scroll para la pantalla

--- EventZezziApp/app/src/main/java/com/zezzi/eventzezziapp/ui/meals/view/IngredientsCategoriesScreen.kt
+++ EventZezziApp/app/src/main/java/com/zezzi/eventzezziapp/ui/meals/view/IngredientsCategoriesScreen.kt
@@ -11,7 +11,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material3.Button
@@ -80,6 +82,8 @@ fun IngredientsCategoriesScreen(dishId: String,navController: NavController) {
                 .padding(it)
                 .fillMaxSize()
                 .background(color = Color(20, 33, 61))
+                .verticalScroll(rememberScrollState()),
+
         ) {
             rememberedIngredients.value.forEach { ingredient ->
 
