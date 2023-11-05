14ec903a252c00d98000568e6c72e5395c62764b
LeivaDiego <lei21752@uvg.edu.gt>
2023-11-04 22:40:59 -0600

    Meal Category from Cloud
    - Refactored getMeals() function to fetch data from firestore cloud

--- EventZezziApp/app/src/main/java/com/zezzi/eventzezziapp/data/repository/MealsRepository.kt
+++ EventZezziApp/app/src/main/java/com/zezzi/eventzezziapp/data/repository/MealsRepository.kt
@@ -1,14 +1,35 @@
 package com.zezzi.eventzezziapp.data.repository
 
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
 import com.zezzi.eventzezziapp.data.networking.MealsWebService
+import com.zezzi.eventzezziapp.data.networking.response.MealResponse
 import com.zezzi.eventzezziapp.data.networking.response.MealsByCategoryResponse
 import com.zezzi.eventzezziapp.data.networking.response.MealsCategoriesResponse
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
 
 class MealsRepository(private val webService: MealsWebService = MealsWebService()) {
+
+    private val db = Firebase.firestore
+
     suspend fun getMeals(): MealsCategoriesResponse {
-        return withContext(Dispatchers.IO) {webService.getMeals()}
+        return withContext(Dispatchers.IO) {
+
+            val categories = db.collection("Categorias").get().await()
+
+            MealsCategoriesResponse(
+                categories.map {
+                    MealResponse(
+                        it.id,
+                        it.getString("categoria") ?: "",
+                        it.getString("descripcion") ?:"",
+                        it.getString("url") ?: ""
+                    )
+                }
+            )
+        }
     }
 
     suspend fun getMealsByCategory(category: String): MealsByCategoryResponse{
