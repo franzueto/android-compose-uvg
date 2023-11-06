044a10b461cca9f173d3c3faff7569b0f881b8e7
jorgelopez <lop221038@uvg.edu.gt>
2023-11-05 19:40:25 -0600

    Lab11/JorgeLopez

--- EventZezziApp/app/build.gradle.kts
+++ EventZezziApp/app/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+
+    // Add the Google services Gradle plugin
+    id("com.google.gms.google-services")
+
 }
 
 android {
@@ -70,6 +74,14 @@ dependencies {
     implementation("com.google.code.gson:gson:2.9.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:32.5.0"))
+    implementation("com.google.firebase:firebase-firestore-ktx:24.9.1")
+
+    // TODO: Add the dependencies for Firebase products you want to use
+    // When using the BoM, don't specify versions in Firebase dependencies
+    implementation("com.google.firebase:firebase-analytics")
 
 
     testImplementation("junit:junit:4.13.2")
--- EventZezziApp/app/src/main/java/com/zezzi/eventzezziapp/data/repository/MealsRepository.kt
+++ EventZezziApp/app/src/main/java/com/zezzi/eventzezziapp/data/repository/MealsRepository.kt
@@ -1,46 +1,72 @@
 package com.zezzi.eventzezziapp.data.repository
 
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.zezzi.eventzezziapp.data.networking.MealsWebService
 import com.zezzi.eventzezziapp.data.networking.response.DishesCategoriesResponse
+import com.zezzi.eventzezziapp.data.networking.response.DishesResponse
 import com.zezzi.eventzezziapp.data.networking.response.IngredientsCategoriesResponse
+import com.zezzi.eventzezziapp.data.networking.response.MealResponse
 import com.zezzi.eventzezziapp.data.networking.response.MealsCategoriesResponse
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
 
 class MealsRepository(private val webService: MealsWebService = MealsWebService()) {
-    suspend fun getMeals(): MealsCategoriesResponse? {
+
+    private val db = Firebase.firestore
+
+    suspend fun getMeals(): MealsCategoriesResponse {
+        return withContext(Dispatchers.IO) {
+            val documents = db.collection("Meals")
+                .get().await().documents
+
+            val mealResponses = documents.map {
+                MealResponse(
+                    it.id,
+                    it.getString("name") ?: "",
+                    it.getString("description") ?: "",
+                    it.getString("imageUrl") ?: ""
+                )
+            }
+
+            MealsCategoriesResponse(mealResponses)
+        }
+    }
+
+
+    suspend fun getDishes(mealName: String): DishesCategoriesResponse {
+        return withContext(Dispatchers.IO) {
+            //Se bajan todos los documentos de la coleccion Meals
+            val documents = db.collection("Meals")
+                .document(mealName).collection("Dishes")
+                .get().await().documents
+
+            DishesCategoriesResponse(
+                documents.map {
+                    DishesResponse(
+                        it.getString("id1") ?: "",
+                        it.getString("name1") ?: "",
+                        it.getString("imageUrl1") ?:
+                        it.id
+                    )
+                }
+            )
+        }
+    }
+
+
+    suspend fun getIngredients(dishId: String): IngredientsCategoriesResponse? {
         return withContext(Dispatchers.IO) {
             try {
-                // Llamar a la función suspendida en el webService para obtener los datos.
-                webService.getMeals()
+                val response = webService.getIngredients(dishId)
+                Result.success(response).getOrNull()
             } catch (e: Exception) {
                 // Manejar errores si es necesario.
                 null
             }
         }
     }
-
-    suspend fun getDishes(mealName:String): DishesCategoriesResponse? {
-        return withContext(Dispatchers.IO) {
-            try {
-                val response = webService.getDishes(mealName)
-                Result.success(response).getOrNull()
-            } catch (e: Exception) {
-                // Manejar errores
-                null
-            }
-        }
-    }
-
-    suspend fun getIngredients(dishId:String): IngredientsCategoriesResponse? {
-        return withContext(Dispatchers.IO){
-            try{
-                val response = webService.getIngredients(dishId)
-                Result.success(response).getOrNull()
-            } catch (e: Exception) {
-                null
-            }
-        }
-    }
 }
+
 
